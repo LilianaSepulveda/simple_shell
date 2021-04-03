96db249c3327eb6206368d4e61cc5ea32fd46931
Yaeluribe <2962@holbertonschool.com>
2021-04-03 09:18:05 -0500

    working parrot

--- Tests/prompt.c
+++ Tests/prompt.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
-    char *buff;
+    char *buff = NULL;
     size_t bufsize = 0;
-    size_t chars;
+    size_t chars = 0;
+	char *chao = "exit\n";
 
-    buff = malloc(bufsize * sizeof(char));
-    if(buff == NULL)
-    {
-        perror("Unable to allocate buffer");
-        exit(1);
-    }
-
-    printf("$");
-    chars = getline(&buff,&bufsize,stdin);
-	printf("You said: %s",buff);
-    printf("%zu characters were read.\n",chars);
+	printf("Use CTRL + D or type 'exit' to exit:\n");
+   	while(chars != EOF)
+	{
+    	printf("$");
+    	chars = getline(&buff,&bufsize,stdin);
+		if (!strcmp(chao, buff))
+		{
+			printf("Bye!\n");
+			break;
+		}
+		printf("You said: %s",buff);
+    	printf("%zu characters were read.\n",chars);
+	}
 	free(buff);
     return(0);
 }
