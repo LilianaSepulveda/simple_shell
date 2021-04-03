ccd475cba97cbfbde07f7b77a6b6de05a166a7ec
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-03 17:39:00 -0500

    test the function strtok with getline

---
+++ Tests/test_strtok.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+    char *buff = NULL;
+    size_t bufsize = 0;
+    size_t chars = 0;
+	char *chao = "exit\n";
+	char *token = NULL;
+
+	printf("Use CTRL + D or type 'exit' to exit:\n");
+   	while(1)
+	{
+    	printf("$");
+    	chars = getline(&buff,&bufsize,stdin);
+		token = strtok(buff, " ");
+		while (token != NULL)
+		{
+			printf("%s\n", token);
+			token = strtok(NULL, " ");
+		}
+		if (chars == EOF)
+		{
+			printf("Bye!\n");
+			break;
+		}
+		if (!strcmp(chao, buff))
+		{
+			printf("Bye!\n");
+			break;
+		}
+		printf("%zu characters were read.\n",chars);
+	}
+	free(buff);
+    return(0);
+}
