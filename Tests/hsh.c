9aefd8716976257bab37257f7a420d62e080ac1e
Yaeluribe <2962@holbertonschool.com>
2021-04-12 11:43:21 -0500

    returned to a more stable version

--- Tests/hsh.c
+++ Tests/hsh.c
@@ -8,8 +8,18 @@ int main(void)
 	ssize_t chars = 0, buffsize = 0;
 	char *buff = NULL, **command = NULL, **Path = NULL, **tpath = NULL, **fullpath = NULL;
 	int _forky = 0;
+	pid_t pid;
+	struct stat st;
 
 	Path = getpath();
+	printf("%s\n", Path[0]);
+	printf("%s\n", Path[1]);
+	printf("%s\n", Path[2]);
+	printf("%s\n", Path[3]);
+	printf("%s\n", Path[4]);
+	printf("%s\n", Path[5]);
+	printf("%s\n", Path[6]);
+
 	while (chars != EOF)
 	{
 		write(1, "$ ", 2);
@@ -21,11 +31,20 @@ int main(void)
 			continue;
 		command = count_tok(buff);
 		tpath = Path;
-		fullpath = add_command(command[0],tpath);
-		_forky = _fork(fullpath, tpath);
-		
+		/*fullpath = add_command(command[0],tpath);*/
+		_forky = fork();
+		if (_forky != 0)
+			wait(0);
+		if (_forky == 0)
+		{
+			if (stat(command[0], &st) == 0)
+				if (execve(command[0], command, environ))
+					perror("Error: ");
+			free(command);
+			exit(127);
+		}
+
 	}
-	free(Path);
 	free(command);
 	free(buff);
 	return (0);
