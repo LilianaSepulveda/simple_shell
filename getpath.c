aab11c4d32e0da2cfcaf2d2e1e2a4ebab5b91ec5
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-14 16:46:06 -0500

    fix strtok2 function and add free tokens function

--- getpath.c
+++ getpath.c
@@ -3,7 +3,7 @@
  *getpath - function to get PATH and tokenize it
  *Return: An array of trings tokenized
  */
-char **getpath(void)
+/*char **getpath(void)
 {
 	char *ptr = NULL, **ptrtoken = NULL;
 
@@ -15,7 +15,7 @@ char **getpath(void)
  *retarray - function get an array
  *Return: the string of all path
  */
-char *retarray(void)
+/*char *retarray(void)
 {
 	int i = 0, j;
 	char *ptr = NULL, *path = "PATH=";
@@ -42,53 +42,44 @@ char **_strtok2(char *path)
 	int i = 0, b = 0, copysize = 0;
 	char **pathtoken = NULL, *copy = NULL, *token = NULL, *copy2 = NULL;
 
-	copysize = _strlen(path);
-	copy = malloc(sizeof(char) * (copysize + 1));
-	if (copy == NULL)
+	while (path[i] != '\0')
 	{
-		free(copy);
-		perror("Error: ");
+		if(':' == path[i])
+		{
+			b++;
+			i++;
+		}
+		i++;
 	}
-	copy2 = malloc(sizeof(char) * (copysize + 1));
-	if (copy2 == NULL)
+	pathtoken = malloc(sizeof(char *) * (b + 1));
+	if (pathtoken == NULL)
 	{
-		free(copy2);
-		perror("Error: ");
+		free(pathtoken);
+		perror("Error:");
 	}
 	copy = _strcpy(copy, path);
-	copy2 = _strcpy(copy2, path);
 	token = strtok(copy, ":");
+	i = 0;
 	while (token)
 	{
-		b++;
-		token = strtok(NULL, ":");
-	}
-	free(copy);
-	pathtoken = (char **)malloc(sizeof(char *) * (b + 1));
-	token = NULL;
-	token = strtok(copy2, ":");
-	while (token)
-	{
-		pathtoken[i] = token;
+		pathtoken[i] = _strcpy(pathtoken[i], token);
 		token = strtok(NULL, ":");
 		i++;
 	}
 	pathtoken[i] = NULL;
-	free(copy2);
+	free(copy);
 	return (pathtoken);
 }
-/*
-*int main(void)
-*{
-* char path[] = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
-* char **strtokenized = NULL;
-* strtokenized = _strtok2(path);
-* printf("%s\n", strtokenized[0]);
-* printf("%s\n", strtokenized[1]);
-* printf("%s\n", strtokenized[2]);
-* printf("%s\n", strtokenized[3]);
-* printf("%s\n", strtokenized[4]);
-* printf("%s\n", strtokenized[5]);
-* return(0);
-*}
-*/
+/**
+ * free_tokens - function to free tokens
+ * @tok_2free: array to free
+ * Return - void
+ */
+void free_tokens(char ** tok_2free)
+{
+	int i = 0;
+
+	while(tok_2free[i])
+		free(tok_2free[i++]);
+	free(tok_2free);
+}
