b7d51c345811e5e0e54cffc9c437382b11d405e4
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-13 21:42:22 -0500

    add malloc to handle memory problems

--- Tests/_strcpy.c
+++ Tests/_strcpy.c
@@ -7,14 +7,23 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, j = 0;
+	int i, j = 0, copysize = 0;
+	char *copy = NULL;
+
+	copysize = _strlen(src);
+	copy = malloc(sizeof(char) * (copysize + 1));
+	if (copy == NULL)
+	{
+		free(copy);
+		perror("Error: ");
+	}
 
 	for (i = 0; src[i] != '\0'; i++)
 		;
 	while (j <= i)
 	{
-		dest[j] = src[j];
+		copy[j] = src[j];
 		j++;
 	}
-	return (dest);
+	return (copy);
 }
